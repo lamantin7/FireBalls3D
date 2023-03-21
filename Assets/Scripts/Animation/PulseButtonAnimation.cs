214732f7071d0a70088f118ec4bea8df8b89b58e
lamantin7 <80760852+lamantin7@users.noreply.github.com>
2023-03-21 19:44:49 +0400

    Curency light effect created

--- Assets/Scripts/Animation/PulseButtonAnimation.cs
+++ Assets/Scripts/Animation/PulseButtonAnimation.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PulseButtonanimation : MonoBehaviour
+public class PulseButtonAnimation : MonoBehaviour
 {
     [SerializeField] private Vector3 _scaleFrom;
     [SerializeField] private Vector3 _scaleTo;
