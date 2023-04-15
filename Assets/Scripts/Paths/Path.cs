a23b4c98612ea28639cff5ec44bbabb32ad4ffcc
lamantin7 <80760852+lamantin7@users.noreply.github.com>
2023-04-15 19:10:34 +0400

    Created path and path drawing

---
+++ Assets/Scripts/Paths/Path.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Paths
+{
+    public class Path:MonoBehaviour
+    {
+        [SerializeField] private PathSegment[] _segments = Array.Empty<PathSegment>();
+        public IReadOnlyList<PathSegment> Segments => _segments;
+    }
+}
