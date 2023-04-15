20789c0035292815fd03a3c98a2213c9b20487dc
lamantin7 <80760852+lamantin7@users.noreply.github.com>
2023-04-15 19:31:34 +0400

    Create path following

---
+++ Assets/Scripts/Paths/PathFollowing.cs
@@ -0,0 +1,57 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Paths
+{
+    public class PathFollowing
+    {
+        private readonly Path _path;
+        private readonly MonoBehaviour _follower;
+        private readonly SOmovePreference _preferences;
+
+        private int _pathSegmentIndex;
+
+        public PathFollowing(Path path, MonoBehaviour follower, SOmovePreference preferences)
+        {
+            _path = path;
+            _follower = follower;
+            _preferences = preferences;
+           
+        }
+        public void MoveToNext()
+        {
+            if (_pathSegmentIndex >= _path.Segments.Count)
+                return;
+
+            PathSegment segment = _path.Segments[_pathSegmentIndex];
+            Transform[] waypoints = segment.WayPoints;
+
+            _follower.StartCoroutine(MoveBetween(waypoints));
+        }
+
+        private IEnumerator MoveBetween(IReadOnlyList<Transform> waypoints)
+        {
+            int index = 1;
+            Transform followerTransform= _follower.transform;
+
+            while(index < waypoints.Count) 
+            {
+                Vector3 position = waypoints[index].position;
+
+                followerTransform
+                    .DOLookAt(position,_preferences.RotateDuration)
+                    .OnComplete(()=>followerTransform
+                    .DOMove(position,_preferences.DurationPerWaypoint));
+                yield return new WaitForSeconds(_preferences.TotalDuration);
+                index++;
+            }
+            _pathSegmentIndex++;
+        }
+    }
+}
---
+++ Assets/Scripts/Paths/SOmovePreference.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Paths
+{
+    [CreateAssetMenu(fileName ="MovePreferences", menuName ="ScriptableObjects/Paths/MovePreferences")]
+    public class SOmovePreference
+    {
+        [SerializeField][Min(0.0f)] private float _durationPerWaypoint;
+        [SerializeField][Min(0.0f)] private float _rotateDuration;
+
+        public float DurationPerWaypoint=> _durationPerWaypoint;
+        public float RotateDuration=> _rotateDuration;
+        public float TotalDuration => _durationPerWaypoint + _rotateDuration;
+    }
+}
