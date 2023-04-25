38ff8339dfba6ca266d8e491b9fbf98bc0616f43
lamantin7 <80760852+lamantin7@users.noreply.github.com>
2023-04-25 20:56:00 +0400

    Refactoring player script

--- Assets/Scripts/Player components/Player.cs
+++ Assets/Scripts/Player components/Player.cs
@@ -11,12 +11,19 @@ namespace PlayerComponents
 
     public class Player : MonoBehaviour
     {
-        [SerializeField] private Character _character;
-        [SerializeField] private SOshootingPreferences _shootingPreferences;
-        [SerializeField] private ProjectilePool _projectilePool;
+        [Header("Characteres")]
+        [SerializeField] private SOCharacterContainer _characterContainer;
 
+        [Header("Path")]
         [SerializeField] private Path _path;
         [SerializeField] private SOmovePreference _movePreference;
+
+        [ Header("Shooting")]
+        [SerializeField] private SOShootingPreferences _shootingPreferences;
+        [SerializeField] private ProjectilePool _projectilePool;
+
+        
+       
         
 
         private FireRate _fireRate;
@@ -26,10 +33,11 @@ namespace PlayerComponents
 
         private void Start()
         {
+            Character character = _characterContainer.Create(transform);
             _projectilePool.Initialize(_shootingPreferences.ProjectileFactory);
             _projectilePool.Prewarm(20);
 
-            _weapon = new Weapon (_character.ShootPoint,_projectilePool,_shootingPreferences.ProjectileSpeed);
+            _weapon = new Weapon (character.ShootPoint,_projectilePool,_shootingPreferences.ProjectileSpeed);
             _fireRate = new FireRate(_shootingPreferences.FireRate);
             _pathFollowing = new PathFollowing(_path, this, _movePreference);;
 
